72c03bb8ea3eebf5c1dfbaf731cbab797cbf8260
Rohit Deep <rohit.deep.dev@gmail.com>
2011-04-28 11:40:06 -0700

    Added variables in test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before (:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +14,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => @base_title + "Home")
     end 
   end
 
@@ -23,7 +26,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + "Contact")
     end 
   end
 
@@ -35,7 +38,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + "About")
     end 
   end
 
@@ -47,7 +50,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_selector("title", :content => @base_title + "Help")
     end 
   end
 end
