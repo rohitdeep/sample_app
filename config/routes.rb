ac035580e7e3709037db28fd700bde4223806598
Rohit Deep <rohit.deep.dev@gmail.com>
2011-04-29 11:40:00 -0700

    Finished first cut at the user model

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-  get "users/new"
+  resources :users
 
   match '/signup', :to => 'users#new'
   
